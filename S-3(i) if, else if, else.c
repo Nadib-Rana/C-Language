5260dfd51472e0ab5767e90062414533452ccfe5
Nadib-Rana <codecrafersnadib@gmail.com>
2024-08-10 12:09:25 +0600

    if, else if, else

---
+++ S-3(i) if, else if, else.c
@@ -0,0 +1,15 @@
+// if, else if, else
+
+#include <stdio.h>
+int main() {
+int number = -1;
+if (number > 0){
+  printf("Positive Number.");
+}
+else if( number < 0){
+  printf("Negative Number");
+}
+else{
+  printf("Zero");
+}
+}
