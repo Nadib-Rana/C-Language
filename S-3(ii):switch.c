321bc4243dff156932dd3ac9eea64592683aad3d
Nadib-Rana <codecrafersnadib@gmail.com>
2024-08-10 12:12:54 +0600

    switch case

---
+++ S-3(ii):switch.c
@@ -0,0 +1,17 @@
+// switch
+
+#include <stdio.h>
+int day = 2;
+int main(){
+switch (day) {
+    case 1:
+        printf("Monday\n");
+        break;
+    case 2:
+        printf("Tuesday\n");
+        break;
+    default:
+        printf("Other day\n");
+        break;
+}
+}
