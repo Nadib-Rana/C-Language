6f8a1e605028dad58535c616d57107b6a520eb9d
Nadib-Rana <codecrafersnadib@gmail.com>
2024-08-10 09:22:46 +0600

    Create S-1(i):DataType.c
    Primitive Data Types

---
+++ S-1(i):DataType.c
@@ -0,0 +1,10 @@
+// Primitive Data type :
+
+// int: Used to store integer values,
+   int age = 25;
+// float: Used to store floating-point numbers,
+   float price = 19.99;
+// char: Used to store single characters, 
+   char letter = 'A';
+// double: Used for double-precision floating-point numbers,
+    double value = 19.99;
